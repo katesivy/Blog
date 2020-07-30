e8555463c48f9bfe3f6f51600ac8788da688dcd6
katesivy <58646769+katesivy@users.noreply.github.com>
2020-07-30 13:03:27 -0400

    adding calculator to portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,7 +10,6 @@ export default function BlogPosts() {
             <div className="jumbotron jumbotron-fluid text-center">
                 <div className="container">
                     <h1 className="display-4">Portfolio</h1>
-                    {/* <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> */}
                 </div>
             </div>
 
@@ -20,16 +19,27 @@ export default function BlogPosts() {
                 <div className="card-body text-center">
                     <h2 className="card-title">What's Cookin'?</h2>
                     <p className="card-text col-6 offset-3">A full-stack application on Google Cloud with a Laravel backend and React frontend allowing users to create and manage their own recipes and view recipes from the database. </p>
-                    <a href="https://recipe-final-project.web.app/"  target="_blank" className="col-3 text-center btn btn-secondary">Final Project</a>
+                    <a href="https://recipe-final-project.web.app/" target="_blank" className="col-3 text-center btn btn-secondary">Final Project</a>
                 </div>
             </div>
+
             <div className="card text-center">
                 <div className="card-header">
                 </div>
                 <div className="card-body text-center">
                     <h2 className="card-title">Kate's Kitchen</h2>
                     <p className="card-text">A sleek design of the most disgusting menu ever.</p>
-                    <a href="https://katesivy.github.io/Restaurant"  target="_blank" className="col-3 text-center btn btn-secondary">React Restaurant</a>
+                    <a href="https://katesivy.github.io/Restaurant" target="_blank" className="col-3 text-center btn btn-secondary">React Restaurant</a>
+                </div>
+            </div>
+
+            <div className="card text-center">
+                <div className="card-header">
+                </div>
+                <div className="card-body text-center">
+                    <h2 className="card-title">To-Do List</h2>
+                    <p className="card-text">Keeps track of your to-do items through JavaScript and React with Bootstrap, HMTL, & CSS.</p>
+                    <a href="https://to-do-list-cfb78.web.app" target="_blank" className="col-3 text-center btn btn-secondary">To-Do List</a>
                 </div>
             </div>
 
@@ -39,7 +49,7 @@ export default function BlogPosts() {
                 <div className="card-body text-center">
                     <h2 className="card-title">Weather App</h2>
                     <p className="card-text">Checks your local weather, using React and API's</p>
-                    <a href="https://ks-weather-app.web.app"  target="_blank" className="col-3 text-center btn btn-secondary">Weather App</a>
+                    <a href="https://ks-weather-app.web.app" target="_blank" className="col-3 text-center btn btn-secondary">Weather App</a>
                 </div>
             </div>
 
@@ -49,10 +59,19 @@ export default function BlogPosts() {
                 <div className="card-body text-center">
                     <h2 className="card-title">Puzzle Slider</h2>
                     <p className="card-text">Click the tiles around the blank tile to see it move. Uses JavaScript and React.</p>
-                    <a href="https://puzzle-slider-734c5.web.app"  target="_blank" className="col-3 text-center btn btn-secondary">Puzzle Slider</a>
+                    <a href="https://puzzle-slider-734c5.web.app" target="_blank" className="col-3 text-center btn btn-secondary">Puzzle Slider</a>
                 </div>
             </div>
 
+            <div className="card text-center">
+                <div className="card-header">
+                </div>
+                <div className="card-body text-center">
+                    <h2 className="card-title">Calculator</h2>
+                    <p className="card-text">Built approximately 98% with JavaScript, but find out for sure using this handy calculator!</p>
+                    <a href="https://katesivy.github.io/JSCalculator/" target="_blank" className="col-3 text-center btn btn-secondary">JavaScript Calculator</a>
+                </div>
+            </div>
 
 
         </>
